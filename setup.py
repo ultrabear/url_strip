7a5806be528669a3835974252eaf46a920f587e8
ultrabear <bearodark@gmail.com>
2022-05-08 14:31:26 -0700

    yapf pass over setup.py

--- setup.py
+++ setup.py
@@ -6,16 +6,16 @@ def get_requires() -> List[str]:
     with open("requirements.txt", "r") as fp:
         return [i.strip("\n") for i in fp.readlines()]
 
+
 def support_pyvers(major: int, minor: range) -> List[str]:
     return [f"Programming Language :: Python :: {major}.{i}" for i in minor]
-        
-
 
 
 setup(
     name="url_strip",
     version="0.0.1",
-    description="Library to strip http urls of tracking elements, and use shorthand variants of urls",
+    description=
+    "Library to strip http urls of tracking elements, and use shorthand variants of urls",
     long_description=open("README.md", "r").read(),
     long_description_content_type="text/markdown",
     author="ultrabear",
@@ -23,12 +23,12 @@ setup(
     packages=find_packages(),
     install_requires=get_requires(),
     classifiers=[
-        *support_pyvers(3, range(8,12)),
+        *support_pyvers(3, range(8, 12)),
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
         "Natural Language :: English",
         "Operating System :: OS Independent",
         "Topic :: Text Processing :: Filters",
         "Typing :: Typed",
-    ],
+        ],
     )
